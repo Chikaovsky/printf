3b94b7ea13e303fea1cc24b187b914333da19e92
Princenico <111049274+Chikaovsky@users.noreply.github.com>
2022-10-21 00:10:21 +0100

    Update write_handlers.c

--- write_handlers.c
+++ write_handlers.c
@@ -36,14 +36,14 @@ int handle_write_char(char c, char buffer[],
 	{
 		buffer[BUFF_SIZE - 1] = '\0';
 		for (i = 0; i < width - 1; i++)
-			buffer[BUFF_SIZE - i - 2] = padd;
+		buffer[BUFF_SIZE - i - 2] = padd;
 
 		if (flags & F_MINUS)
-			return (write(1, &buffer[0], 1) +
-					write(1, &buffer[BUFF_SIZE - i - 1], width - 1));
+		return (write(1, &buffer[0], 1) +
+			write(1, &buffer[BUFF_SIZE - i - 1], width - 1));
 		else
-			return (write(1, &buffer[BUFF_SIZE - i - 1], width - 1) +
-					write(1, &buffer[0], 1));
+		return (write(1, &buffer[BUFF_SIZE - i - 1], width - 1) +
+			write(1, &buffer[0], 1));
 	}
 
 	return (write(1, &buffer[0], 1));
